54615e524c26e6df32780ad036eb871523025d80
Pranto408 <prantodatta408@gmail.com>
2026-02-19 13:49:57 +0600

    Log in funtonal

--- scripts/login.js
+++ scripts/login.js
@@ -1,1 +1,17 @@
-console.log("Working")
+document.getElementById("login-btn").addEventListener("click", function () {
+  const mobileNumber = document.getElementById("input-mobile");
+  const numberValue = mobileNumber.value;
+  console.log(numberValue);
+
+  const pinNumber = document.getElementById("input-pin");
+  const pinValue = pinNumber.value;
+  console.log(pinValue);
+
+  if (numberValue == "01234567890" && pinValue == "1234") {
+      alert("Login Successful");
+      window.location.assign("./home.html");
+  } else {
+    alert("Login Failed");
+    return;
+  }
+});
